c827944597d575776fb4cc1444791931254aee31
AbeeraUmair <aleemabeera@gmail.com>
2025-06-13 14:19:32 +0500

    update with cloudinary

--- components/team.tsx
+++ components/team.tsx
@@ -2,8 +2,6 @@
 
 import { useRef } from "react"
 import { motion, useInView } from "framer-motion"
-import linkedIn from "../public/icons/linkedIn.png"
-import github from "../public/icons/github.png"
 import Link from "next/link"
 import Image from "next/image"
 
@@ -88,10 +86,10 @@ export function Team() {
                 <p className="text-gray-600 mb-4">{member.bio}</p>
                 <div className="flex space-x-3 ">
                   <Link href={member.linkedin} target="blank" className="text-gray-400">
-                    <Image src={linkedIn} alt="linked-in icon" className="h-5 w-5" />
+                    <Image src="https://res.cloudinary.com/dqkt0g0he/image/upload/v1749749249/linkedIn_qwcwfs.png" alt="linked-in icon" className="h-5 w-5" />
                   </Link>
                   <Link href={member.github} target="blank" className="text-gray-400">
-                  <Image src={github} alt="github-icon" className="h-6 w-6" />
+                  <Image src="https://res.cloudinary.com/dqkt0g0he/image/upload/v1749749248/github_nqa1zy.png" alt="github-icon" className="h-6 w-6" />
                   </Link>
                 </div>
               </div>
