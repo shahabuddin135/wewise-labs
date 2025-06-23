c7d8483dbbe16dafa7adad6a80c27f915f5c937c
Saba Sarfaraz <sarfarazsaba11@gmail.com>
2025-06-23 22:10:31 +0500

    revamp sitemap page, add light and dark theme, adjust color according to backgroud

--- app/sitemap/page.tsx
+++ app/sitemap/page.tsx
@@ -8,9 +8,9 @@ export const metadata = {
 
 export default function SiteMapPage() {
   return (
-    <main className="min-h-screen flex flex-col">
-      <div className="flex-1 pt-24 pb-16 px-4 md:px-8 max-w-7xl mx-auto w-full">
-        <h1 className="text-3xl md:text-4xl font-bold mb-8">Site Map</h1>
+    <main className="min-h-screen flex flex-col max-w-7xl mx-auto mt-36">
+      <div className="flex-1 pb-8 px-4 md:px-8 max-w-7xl mx-auto w-full ">
+        <h1 className="text-3xl font-bold mb-4 md:text-6xl px-4 dark:text-white">Site Map</h1>
         <SiteMapContent />
       </div>
       <Footer />
