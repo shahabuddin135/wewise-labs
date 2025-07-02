122ca46a0d1228b3179a90ecc37106988fc07ee0
AbeeraUmair <aleemabeera@gmail.com>
2025-07-03 02:15:50 +0500

    Update images in WhyChooseUs component for improved visual appeal

--- components/why-choose-us.tsx
+++ components/why-choose-us.tsx
@@ -2,32 +2,32 @@ import { AnimatedTestimonials } from "./animated-choose-cards";
 
 const reasons = [
   {
-    icon:"https://res.cloudinary.com/dqkt0g0he/image/upload/v1751275355/WhatsApp_Image_2025-06-30_at_14.06.06_7701946e_xh4mnm.jpg",
+    icon:"https://res.cloudinary.com/dqkt0g0he/image/upload/v1751489311/ChatGPT_Image_Jul_2_2025_11_22_18_PM_w4mir7.png",
     title: "Quality Assurance",
     description: "We follow rigorous testing and quality assurance processes to deliver bug-free applications.",
   },
   {
-    icon: "https://res.cloudinary.com/dqkt0g0he/image/upload/v1751275364/WhatsApp_Image_2025-06-30_at_13.39.54_87c62b8a_vdjq5f.jpg",
+    icon: "https://res.cloudinary.com/dqkt0g0he/image/upload/v1751489309/ChatGPT_Image_Jul_2_2025_11_22_16_PM_rlmxbp.png",
     title: "Secure Development",
     description: "Security is built into our development process from the ground up.",
   },
   {
-    icon: "https://res.cloudinary.com/dqkt0g0he/image/upload/v1751277613/WhatsApp_Image_2025-06-30_at_14.59.51_da5cf479_i2we1y.jpg",
+    icon: "https://res.cloudinary.com/dqkt0g0he/image/upload/v1751489292/ChatGPT_Image_Jul_2_2025_11_22_26_PM_gyhuty.png",
     title: "Performance Focused",
     description: "We optimize every aspect of your application for maximum speed and efficiency.",
   },
   {
-    icon: "https://res.cloudinary.com/dqkt0g0he/image/upload/v1751276647/WhatsApp_Image_2025-06-30_at_14.29.41_f4a8a66d_vl9o0b.jpg",
+    icon: "https://res.cloudinary.com/dqkt0g0he/image/upload/v1751490329/ChatGPT_Image_Jul_3_2025_02_04_56_AM_vixqjk.png",
     title: "Collaborative Approach",
     description: "We work closely with you throughout the development process.",
   },
   {
-    icon: "https://res.cloudinary.com/dqkt0g0he/image/upload/v1751275457/WhatsApp_Image_2025-06-30_at_13.21.41_c2de7c88_gatkdv.jpg",
+    icon: "https://res.cloudinary.com/dqkt0g0he/image/upload/v1751489326/ChatGPT_Image_Jul_2_2025_11_22_40_PM_uj28yc.png",
     title: "Timely Delivery",
     description: "We respect deadlines and deliver projects on time, every time.",
   },
   {
-    icon: "https://res.cloudinary.com/dqkt0g0he/image/upload/v1751276853/WhatsApp_Image_2025-06-30_at_14.47.18_ec1e74bb_iqnscw.jpg",
+    icon: "https://res.cloudinary.com/dqkt0g0he/image/upload/v1751489331/ChatGPT_Image_Jul_3_2025_01_31_09_AM_np1qph.png",
     title: "Industry Expertise",
     description: "Our team brings years of experience across various industries.",
   },
