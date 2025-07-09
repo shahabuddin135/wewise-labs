353241d0c6bf168f9fe4f93f4b21a18b48c31ee9
shahabuddin135 <voyagersvrs135@gmail.com>
2025-07-09 23:56:59 +0500

    nav wrapper and testimonials

---
+++ sanity/schemaTypes/testimonial.ts
@@ -0,0 +1,56 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'testimonial',
+  title: 'Testimonial',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'title',
+      title: 'Title/Position',
+      type: 'string',
+    }),
+    defineField({
+      name: 'company',
+      title: 'Company',
+      type: 'string',
+    }),
+    defineField({
+      name: 'testimonial',
+      title: 'Testimonial',
+      type: 'text',
+      validation: (Rule) => Rule.required().max(500),
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'company',
+      media: 'image',
+    },
+    prepare(selection) {
+      const { subtitle } = selection
+      return {
+        ...selection,
+        subtitle: subtitle ? `${subtitle}` : '',
+      }
+    },
+  },
+}) 
