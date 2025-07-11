f882e2c289cfeb38b9b8206ec1de187437783e3e
AbeeraUmair <aleemabeera@gmail.com>
2025-07-12 04:48:16 +0500

    Add react-responsive and update styles for horizontal scrolling

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { useTheme } from 'next-themes';
 import Image from "next/image"
+import { Footer } from '@/components/Layout/footer';
 
 const Careers = () => {
   const { theme } = useTheme();
@@ -20,6 +21,7 @@ const Careers = () => {
         className='mt-6'
       />
       <h2 className="text-3xl text-center md:text-5xl font-heading font-bold mb-6 dark:text-white">Coming Soon!</h2>
+      <Footer/>
 
     </div>
   )
