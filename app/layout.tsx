7c1258c8705d78920126620de3031abd0f366008
AbeeraUmair <aleemabeera@gmail.com>
2025-08-25 01:23:28 +0500

    Add script of Google Tag Manager in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,7 +79,7 @@ export default function RootLayout({
         {/* You can keep a default favicon as a fallback */}
         <link rel="icon" href="/favicon-light.ico" />
        
-        {/* Google tag (gtag.js)  */}
+        {/* Google Analytics tag (gtag.js)  */}
         <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}></script>
         <script
            dangerouslySetInnerHTML={{
@@ -91,8 +91,25 @@ export default function RootLayout({
               `
            }}
          />
+         
+         {/* Google Tag Manager */}
+            <script>
+                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-NX5QRW9Q');`}
+            </script>
+           {/* End Google Tag Manager  */}
+
       </head>
       <body className={`dark:bg-gray-950 ${ptSansCaption.variable}  ${pacifico.variable}  ${kablammo.variable} ${archivo.variable} ${nunito.variable} ${ubuntu.variable} ${arizonia.variable} ${Libre.variable} font-sans bg-white text-black antialiased`}>
+ 
+         {/* Google Tag Manager (noscript) */}
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NX5QRW9Q"
+            height="0" width="0" style={{"display":"none","visibility":"hidden"}}></iframe></noscript>
+         {/* End Google Tag Manager (noscript) */}
+
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
         <ScrollHandler/>
         <NavbarWrapper />
